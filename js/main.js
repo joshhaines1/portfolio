9a83b563d1fc25c162990e6c48cbb2927f39c901
Josh Haines <joshhaines1@icloud.com>
2025-12-25 17:07:46 -0600

    Updated stats section and removed loading bar

--- js/main.js
+++ js/main.js
@@ -10,19 +10,19 @@
 	"use strict";
 
 	/*---------------------------------------------------- */
-	/* Preloader
+	/* Preloader - Disabled
 	------------------------------------------------------ */ 
-   $(window).load(function() {
+   // $(window).load(function() {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+   //    // will first fade out the loading animation 
+   //  	$("#loader").fadeOut("slow", function(){
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+   //     // will fade out the whole DIV that covers the website.
+   //     $("#preloader").delay(300).fadeOut("slow");
 
-      });       
+   //   });       
 
-  	})
+  	// })
 
 
   	/*---------------------------------------------------- */
